GNOME/swell-foop

commit f9dcfa1319656fba096a792f258ed15f06f52f40

Small optimization.

src/game-view.vala
@@ -343,6 +343,7 @@ private class GameGroup : Clutter.Group
     /* Move Keyboard cursor */
     internal void cursor_move (int x, int y)
     {
+        // update abstract cursor coords
         if (cursor_active)
         {
             int old_cursor_x = cursor_x;
@@ -358,12 +359,21 @@ private class GameGroup : Clutter.Group
         else
             cursor_active = true;
 
-        opacity_for_connected_tiles (highlighted, Opacity.HALF);
-        highlighted = find_tile_at_position (cursor_x, cursor_y);
+        // highlight and unhighlight
+        TileActor? cursor = find_tile_at_position (cursor_x, cursor_y);
 
-        if (highlighted != null)
-            opacity_for_connected_tiles (highlighted, Opacity.FULL);
+        if ((highlighted != null && cursor == null)
+         || (highlighted == null && cursor != null)
+         || (highlighted != null && cursor != null && ((!) highlighted).tile.color != ((!) cursor).tile.color))
+        {
+            // opacity_for_connected_tiles() handles correctly a null TileActor
+            opacity_for_connected_tiles (highlighted, Opacity.HALF);
+            opacity_for_connected_tiles (cursor,      Opacity.FULL);
+        }
 
+        highlighted = cursor;
+
+        // update visual cursor position
         float xx, yy;
         xx = cursor_x * tile_size;
         yy = (game.rows - 1 - cursor_y) * tile_size;

commit b124eb232d165bfe2ca8fd19a5579e6dc526bd7d

Fix cursor bug.
Commit f9dcfa1319 broke the cursor.

src/game-view.vala
@@ -358,18 +358,18 @@ private class GameGroup : Clutter.Group
             cursor_active = true;
 
         // highlight and unhighlight
-        TileActor? cursor = find_tile_at_position (cursor_x, cursor_y);
+        TileActor? cursor_tile = find_tile_at_position (cursor_x, cursor_y);
 
-        if ((highlighted != null && cursor == null)
-         || (highlighted == null && cursor != null)
-         || (highlighted != null && cursor != null && ((!) highlighted).tile.color != ((!) cursor).tile.color))
+        if ((highlighted != null && cursor_tile == null)
+         || (highlighted == null && cursor_tile != null)
+         || (highlighted != null && cursor_tile != null && ((!) highlighted).tile.color != ((!) cursor_tile).tile.color))
         {
             // opacity_for_connected_tiles() handles correctly a null TileActor
             opacity_for_connected_tiles (highlighted, Opacity.HALF);
-            opacity_for_connected_tiles (cursor,      Opacity.FULL);
+            opacity_for_connected_tiles (cursor_tile, Opacity.FULL);
         }
 
-        highlighted = cursor;
+        highlighted = cursor_tile;
 
         // update visual cursor position
         float xx, yy;